261494048d96ae2cfe261ce013f90dc6d0015413
Alan Coleman <boomfelled@gmail.com>
2024-04-17 14:43:09 +0100

    3rd tutorial login process in place, logout not working

--- hackershack_website/settings.py
+++ hackershack_website/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django Auth Settings
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "index"
+#LOGOUT_REDIRECT_URL = "index"
--- hackershack_website/urls.py
+++ hackershack_website/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for hackershack_website project.
+"""hackershack_website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,12 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-#from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('about', views.about, name="about"),
-    path('contact', views.contact, name="contact")
+    path('contact', views.contact, name="contact"),
+    path('accounts/profile', views.ProfileView.as_view(), name="profile"),
+
+    # Django Auth
+    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
 ]
+
+# path('accounts/', include('django.contrib.auth.urls'))
+
+
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
